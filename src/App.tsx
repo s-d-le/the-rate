90a80f73602800179836fcbc5be12defd9b10a2f
Son Le <son.le@intersport.com>
2021-09-15 19:23:50 +0200

    currency list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import CurrencyList from "currency-list";
 
 function App() {
+  const [base, setBase] = useState<string>("");
+  const [target, setTarget] = useState<string>("");
+  const exchangeRate = `${process.env.REACT_APP_ALPHA_URL}/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${base}&to_currency=${target}&apikey=${process.env.REACT_APP_ALPHA_KEY}`;
+
+  const currencyList = CurrencyList.getAll("en_US");
+
+  const currencyDropDown = () => {
+    return (
+      <>
+        {Object.keys(currencyList).map((keyName) => (
+          <li className="travelcompany-input" key={keyName}>
+            <span className="input-label">Name: {keyName}</span>
+          </li>
+        ))}
+      </>
+    );
+  };
+
   return (
     <div className="App">
       <h1>Hello there</h1>
+      {currencyDropDown()}
     </div>
   );
 }
