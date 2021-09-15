e17697281270d56a6422cb2703df301db628e2d5
Son Le <son.le@intersport.com>
2021-09-15 23:59:06 +0200

    can get rate

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import "./App.css";
 import CurrencyList from "currency-list";
 
 interface ICurrencyDropDown {
+  placeholder: string;
   currencyList: {};
   selectedCurrency: string;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
 const CurrencyDropDown: FC<ICurrencyDropDown> = ({
+  placeholder,
   currencyList,
   selectedCurrency,
+  onChange,
 }) => {
   return (
-    <select value={selectedCurrency}>
+    <select value={selectedCurrency} onChange={onChange}>
+      <option value="" disabled hidden>
+        {placeholder}
+      </option>
       {Object.keys(currencyList).map((keyName) => (
-        <option key={keyName} value="keyName">
+        <option key={keyName} value={keyName}>
           {keyName}
         </option>
       ))}
@@ -25,18 +32,45 @@ const CurrencyDropDown: FC<ICurrencyDropDown> = ({
 function App() {
   const [base, setBase] = useState<string>("");
   const [target, setTarget] = useState<string>("");
+  //Let's not multiply by zero
+  const [rate, setRate] = useState<number>(1);
   const exchangeRate = `${process.env.REACT_APP_ALPHA_URL}/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${base}&to_currency=${target}&apikey=${process.env.REACT_APP_ALPHA_KEY}`;
 
   const currencyList = CurrencyList.getAll("en_US");
 
-  const setCurrency = () => {};
+  const sendIt = async () => {
+    try {
+      await fetch(exchangeRate)
+        .then((response) => console.log(response))
+        .then((data) => console.log(data));
+    } catch (error) {
+      console.log("😱 Error: ", error);
+    }
+  };
 
   return (
     <div className="App">
       <h1>Base</h1>
-      <CurrencyDropDown currencyList={currencyList} selectedCurrency={base} />
+      <CurrencyDropDown
+        placeholder="Select your base currency"
+        currencyList={currencyList}
+        selectedCurrency={base}
+        onChange={(event) => {
+          event.preventDefault();
+          setBase(event.target.value);
+        }}
+      />
       <h1>Target</h1>
-      <CurrencyDropDown currencyList={currencyList} selectedCurrency={target} />
+      <CurrencyDropDown
+        placeholder="Select your target currency"
+        currencyList={currencyList}
+        selectedCurrency={target}
+        onChange={(event) => {
+          event.preventDefault();
+          setTarget(event.target.value);
+        }}
+      />
+      <button onClick={sendIt}>Get rate</button>
     </div>
   );
 }
